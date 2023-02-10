e7d4747e6b17a6b613e6ab9664eb9631b2da34c9
Connor <connorcason@gmail.com>
2023-02-09 21:17:52 -0500

    connection to node successful

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # plasma
 Lightweight LND client and utilities for node operation
+
+
+# Troubleshooting
+
+## UmbrelOS
+`lnd` documentation lists `localhost:8080` as the default host serving REST
+requests, but that host is changed to `umbrel.local:8080` if you are running
+UmbrelOS
--- __main__.py
+++ __main__.py
@@ -1,13 +1,21 @@
 import base64, codecs, logging, os, json, requests, sys
 
 logging.basicConfig()
-log = logging.getLogger(__name__)
+log = logging.getLogger()
 log.setLevel(logging.INFO)
+
 
 REST_HOST = os.environ.get('LND_REST_HOST')
 MACAROON_PATH = os.environ.get('LND_MACAROON_PATH')
 TLS_PATH = os.environ.get('LND_TLS_PATH')
 
+def getinfo():
+    log.info('Retreiving node information from /getinfo...')
+    url = f'https://{REST_HOST}/v1/getinfo'
+    macaroon = codecs.encode(open(MACAROON_PATH, 'rb').read(), 'hex')
+    headers = {'Grpc-Metadata-macaroon': macaroon}
+    r = requests.get(url, headers=headers, verify=TLS_PATH)
+    return json.dumps(r, indent=2))
 
 
 if __name__ == "__main__":
@@ -19,12 +27,4 @@ if __name__ == "__main__":
         $TLS_PATH = {TLS_PATH}
     '''
     )
-    url = f'https://{REST_HOST}/v1/getinfo'
-    log.info(MACAROON_PATH)
-    file_like = open(MACAROON_PATH, 'rb')
-    print(file_like)
-    print(type(file_like))
-    macaroon = codecs.encode(open(MACAROON_PATH, 'rb').read(), 'hex')
-    headers = {'Grpc-Metadata-macaroon': macaroon}
-    r = requests.get(url, headers=headers, verify=TLS_PATH)
-    print(r.json())
+    print(getinfo())
